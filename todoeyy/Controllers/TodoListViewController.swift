8881876b7a05f6f0f83fc0c1d6abddc785bddb1e
Milinda Sandaruwan <milindasandaruwan@faique-macbook-pro.local>
2019-04-02 09:51:57 +0530

    MVC

--- todoeyy/Controllers/TodoListViewController.swift
+++ todoeyy/Controllers/TodoListViewController.swift
@@ -9,20 +9,40 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-var item=["item1","item2","item3"]
+var item=[Item]()
+let defaults=UserDefaults.standard
   
     override func viewDidLoad() {
         super.viewDidLoad()
+        let newItem=Item()
+        newItem.title="item1"
+        item.append(newItem)
+        
+        let newItem1=Item()
+        newItem1.title="item1"
+        item.append(newItem1)
+        
+        let newItem2=Item()
+        newItem2.title="item1"
+        item.append(newItem2)
+        
+        
+        
+        
+       /*if let items=defaults.array(forKey: "ToDoListArray") as? [String]
+       {
+        item=items
+        }*/
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return item.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for:indexPath)
-        cell.textLabel?.text=item[indexPath.row]
+        cell.textLabel?.text=item[indexPath.row].title
         return cell
     }
     
@@ -48,7 +68,11 @@ var item=["item1","item2","item3"]
           var nItem=UITextField()
         let alert=UIAlertController(title: "add new", message: "", preferredStyle: .alert)
         let action=UIAlertAction(title: "add item", style: .default) { (action) in
-            self.item.append(nItem.text!)
+            let newItem=Item()
+            newItem.title=nItem.text!
+            
+            self.item.append(newItem)
+            self.defaults.set(self.item,forKey:"ToDoListArray")
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
---
+++ todoeyy/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  todoeyy
+//
+//  Created by Milinda Sandaruwan on 4/1/19.
+//  Copyright © 2019 WIZBIZ International. All rights reserved.
+//
+
+import Foundation
+class Item
+{
+    var title:String=""
+    var done:Bool=false
+}
